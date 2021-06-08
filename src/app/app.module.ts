d191831e27647807e57800abfe0c9c6c8ceeba94
Beros <59673191+Panosmpero@users.noreply.github.com>
2021-06-08 12:36:52 +0100

    added server component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from "@angular/forms"
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+// the extension .ts is added from our webpack which bundles our project
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+// this is called a DECORATOR
+@Component({
+  selector: "app-server",
+  templateUrl: "./server.component.html"
+})
+
+export class ServerComponent {
+
+}
