22a2feb31208f905bd339cacaaa92ece410d0ad0
Beros <59673191+Panosmpero@users.noreply.github.com>
2021-06-09 01:12:07 +0100

    added nested components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 // the extension .ts is added from our webpack which bundles our project
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
