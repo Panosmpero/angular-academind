350e7cf05ff625870ef6e0117f07e0736a180bf4
Beros <59673191+Panosmpero@users.noreply.github.com>
2021-06-10 02:02:11 +0100

    ngFor ngStyle ngClass + assignment

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -12,11 +12,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-  allowNewServer = false;
-  serverCreationStatus = 'No server was created';
-  serverName = 'Test server';
-  username = '';
-  serverCreated = false;
+  allowNewServer: boolean = false;
+  serverCreationStatus: string = 'No server was created';
+  serverName: string = 'Test server';
+  username: string = '';
+  serverCreated: boolean = false;
+  servers: string[] = ['Testserver', 'Testserver 2'];
+  showSecret: boolean = true;
+  clicks = [];
 
   constructor() {
     setTimeout(() => {
@@ -27,6 +30,7 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {}
 
   onCreateServer() {
+    this.servers.push(this.serverName);
     this.serverCreated = true;
     this.serverCreationStatus = `Server ${this.serverName} was created!`;
   }
@@ -34,5 +38,11 @@ export class ServersComponent implements OnInit {
   onUpdateServerName(e: Event) {
     console.log(e);
     this.serverName = (<HTMLInputElement>e.target).value;
+  }
+
+  toggleSecret() {
+    this.showSecret = !this.showSecret;
+    const newDate = new Date().toISOString();
+    this.clicks.push(newDate);
   }
 }
