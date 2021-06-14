6d6134b6f58ff2f48582a6ac28c06742f5adfc0d
Beros <59673191+Panosmpero@users.noreply.github.com>
2021-06-14 22:43:30 +0100

    added global dropdown listener

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,14 +1,24 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+} from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]',
 })
 export class DropdownDirective {
-  @HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen;
+  // @HostListener('click') toggleOpen() {
+  //   this.isOpen = !this.isOpen;
+  // }
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target);
+    // console.log(this.elRef.nativeElement);
+    // console.log(event.target);
   }
 
   @HostBinding('class.open') isOpen = false;
 
-  constructor() {}
+  constructor(private elRef: ElementRef) {}
 }
